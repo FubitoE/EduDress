4664f77f049ba21956b5a6d11cce24d136888e29
FubitoE <shiyu.jun.enzo@gmail.com>
2024-11-29 09:53:29 +0900

    データベースと問題関係画面

--- dressproject/dress/admin.py
+++ dressproject/dress/admin.py
@@ -1,19 +1,30 @@
 from django.contrib import admin
-from .models import Question, Questionimg, Parts, Style, ExamYear
+from .models import Question, Questionimg, Parts, Style, ExamYear, QuestionPart
 
 @admin.register(ExamYear)
 class ExamYearAdmin(admin.ModelAdmin):
     list_display = ['name']
 
+
+# QuestionPart のインライン編集設定
+class QuestionPartInline(admin.TabularInline):
+    model = QuestionPart
+    extra = 1
+    fields = ('text', 'image', 'position', 'order')
+    ordering = ['order']
+
+
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('questions_id', 'questions_text', 'difficulty', 'exam_year')
     search_fields = ('questions_text', 'questions_id')
+    inlines = [QuestionPartInline]  # QuestionPart をインラインで表示
 
     def get_readonly_fields(self, request, obj=None):
         if not request.user.is_superuser:
             return ('questions_id',)
         return ()
+
 
 @admin.register(Questionimg)
 class QuestionImgAdmin(admin.ModelAdmin):
@@ -25,9 +36,11 @@ class QuestionImgAdmin(admin.ModelAdmin):
             return ('id',)
         return ()
 
+
 @admin.register(Parts)
 class PartsAdmin(admin.ModelAdmin):
     list_display = ('parts_id', 'parts_name', 'parts_category', 'parts_default', 'parts_image', 'created_at', 'updated_at')
+
 
 @admin.register(Style)
 class StyleAdmin(admin.ModelAdmin):
